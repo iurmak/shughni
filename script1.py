9e3e11775bc125780e1996c280ade4ab6a5de3f7
iurmak <55744757+iurmak@users.noreply.github.com>
2020-04-12 01:54:27 +0300

    Скрипт, переделывающий список глаголов из Карамшоева (verbs0.txt) в базу глаголов в нашем формате.

--- script1.py
+++ script1.py
@@ -1,89 +1,76 @@
-#[0]* vrblemma, [1] praesstem, [2] praes3sg, [3] pastmasc, [4] pastfepl, [5] perfmasc/perf, [6] perffemn, [7] perfplur, [8] infinite/infinitiveMasc, [9]infinitiveFem (в будущем можем прикрутить [9] imperate, [10] causativ; пока не добавляем)
 def cleaning(str): #deletes all the unneeded punctuation and lowers the string
-    for punct in '=:;.,':
+    for punct in '=:;.,()?':
         str = str.replace(punct,'').lower()
     return str
 
-with open('verbs0.txt', encoding='utf-8') as f:
-    text = f.read()
-text = text.split('\n')
-res = []
-result = []
-while '' in text:
-    text.remove('') # text — лист из словарных статей глаголов, где каждая статья — строка (пустые строки удалены)
-for i in range(len(text)):
-    b = text[i].split() #b — конкретная СС, рассплитенная по пробелам
-    #print(b)
-    if len(b) <= 4:
-        continue
-    else:
-        sm = ['0','1','2','3','4','5','6','7','8','9'] #наш формат данных
-        prom1 = [] #praesstem
-        prom3 = [] #pastmasc или просто универсальная paststem
-        prom4 = [] #PastStemFem
-        switcher = 0
-        if b[0].endswith(':') or b[0].endswith(';'): #если первое слово в СС кончается на : или ;, то в [1] идет только оно
-            prom1.append(b[0])
-        elif b[0].endswith(','): #добавляет все варианты для [1]
-            prom1.append(b[0])
-            for z in range(1,5):
-                if b[z].endswith(',') and switcher == 0:
-                    if b[z+1].startswith('Perf') or b[z+1].startswith('3Sg') or b[z+1].startswith('Inf') or b[z+1].startswith('ж.'):
-                        switcher = 1
-                    prom1.append(b[z])
-                elif switcher == 0:
-                    prom1.append(b[z])
-                    if b[z+1] == 'ж.':
-                        switcher = 1
-        sm[1] = '/'.join(prom1)
-        switcher = 0
-        if len(prom1) > 1:
-            find = b.index(prom1[-1])
-        else:
-            find = 0
-        prom3.append(b[find+1])
-        for z in range(1,4):
-            if b[find+1+z].startswith('Per') or b[find+1+z].startswith('3Sg') or b[find+1+z].startswith('In'):
-                switcher = 1
-            if b[find+1+z].endswith(',') or b[find+1+z].endswith(';') or b[find+1+z].endswith(':') and switcher == 0:
-                prom3.append(b[find+1+z])
-            elif b[find+1+z] == 'ж.':
-                switcher = 1
-                prom4.append(b[find+2+z])
-        sm[3] = '/'.join(prom3)
-        if prom4 == []:
+def extract(gloss,place,source,sample): #extracts verb forms from the verb lists and puts them in their places
+    if gloss in source:
+        n = source.index(gloss)
+        if n == -1:
             pass
         else:
-            sm[4] = '/'.join(prom4)
-        if '3SgPraes' in b:
-            t = b.index('3SgPraes')
-            sm[2] = b[t+1]
-        if 'PerfM' in b:
-            t = b.index('PerfM')
-            sm[5] = b[t+1]
-        if 'Perf' in b:
-            t = b.index('Perf')
-            sm[5] = b[t+1]
-        if 'PerfF' in b:
-            t = b.index('PerfF')
-            sm[6] = b[t+1]
-        if 'PerfPl' in b:
-            t = b.index('PerfPl')
-            sm[7] = b[t+1]
-        if 'Inf' in b:
-            t = b.index('Inf')
-            sm[8] = b[t+1]
-        if 'InfM' in b:
-            t = b.index('InfM')
-            sm[8] = b[t+1]
-        if 'InfF' in b:
-            t = b.index('InfF')
-            sm[9] = b[t+1]
-        res.append(sm)
-for i in res:
-    i = '#'.join(i)
-    i = cleaning(i)
-    result.append(i)
+            sample[place] = source[n+1]
+    return sample
 
-with open('v_ordered.txt', 'w', encoding='utf-8') as f:
-    f.write('\n'.join(result))
+def main():
+    format = {0:('Praes','PraesM'),1:'PraesF',2:'3SgPraes',3:('Past','PastM'),4:'PastF',5:('PerfM','Perf'),6:'PerfF',7:'PerfPl',8:('Inf','InfM'),9:('InfF')} #our format of keeping lemmas/forms
+    with open('verbs0.txt', encoding='utf-8') as f:
+        text = f.read()
+    text = text.split('\n')
+    res = []
+    result = []
+    while '' in text:
+        text.remove('') # text — лист из словарных статей глаголов, где каждая статья — строка (пустые строки удалены)
+    for i in range(len(text)):
+        b = text[i].split() #b — конкретная СС, рассплитенная по пробелам
+        sm = ['0','1','2','3','4','5','6','7','8','9']
+        for pl, gl in format.items():
+            if isinstance(gl,tuple):
+                gl1,gl2 = gl
+                sm = extract(gl1,pl,b,sm)
+                sm = extract(gl2,pl,b,sm)
+            else:
+                sm = extract(gl,pl,b,sm)
+        res.append(sm)
+
+    for i in range(len(res)):
+        for z in range(len(res[i])):
+            if '(' and '/' in res[i][z]:
+                spli = res[i][z].split('/')
+                for p in range(len(spli)):
+                    while '(' in spli[p]:
+                        a = spli[p].replace('(','',1).replace(')','',1)
+                        num = spli[p].find('(') + 1
+                        spli[p] = spli[p][:num] + spli[p][num+1:]
+                        b = spli[p].replace('(','',1).replace(')','',1)
+                        c = [a,b]
+                        spli[p] = '/'.join(c)
+                res[i][z] = '/'.join(spli)
+            elif '(' in res[i][z]:
+                a = res[i][z].replace('(','',1).replace(')','',1)
+                num = res[i][z].find('(') + 1
+                res[i][z] = res[i][z][:num] + res[i][z][num+1:]
+                b = res[i][z].replace('(','',1).replace(')','',1)
+                c = [a,b]
+                res[i][z] = '/'.join(c)
+
+    for i in res:
+        i = '#'.join(i)
+        i = cleaning(i)
+        result.append(i)
+
+    for i in range(len(result)):
+        result[i] = list(result[i])
+        for z in range(len(result[i])):
+            if z != len(result[i]) and result[i][z] == '/':
+                if result[i][z+1] == '/':
+                    result[i][z+1] = ''
+                    result[i] = ''.join(result[i])
+                    print(result[i])
+        result[i] = ''.join(result[i])
+
+    with open('v_ordered.txt', 'w', encoding='utf-8') as f:
+        f.write('\n'.join(result))
+
+if __name__ == '__main__':
+    main()
